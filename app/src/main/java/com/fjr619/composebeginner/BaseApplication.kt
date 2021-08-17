30cfe664b002a2115aa1f61b249ae6e0666f01aa
Franky Wijanarko <frank.jr.619@gmail.com>
2021-08-17 14:22:02 +0700

    hilt dependency injection

---
+++ app/src/main/java/com/fjr619/composebeginner/BaseApplication.kt
@@ -0,0 +1,8 @@
+package com.fjr619.composebeginner
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication: Application() {
+}
--- app/src/main/java/com/fjr619/composebeginner/MainActivity.kt
+++ app/src/main/java/com/fjr619/composebeginner/MainActivity.kt
@@ -23,8 +23,18 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.fjr619.composebeginner.ui.theme.ComposeBeginnerTheme
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var app: BaseApplication
+
+    @Inject
+    lateinit var randomString: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContent {
@@ -38,6 +48,8 @@ class MainActivity : AppCompatActivity() {
 //        }
 
         setContentView(R.layout.activity_main)
+        println("onCreate: the app context: ${app}")
+        println("onCreate: ${randomString}")
     }
 }
 
