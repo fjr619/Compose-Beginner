056a346b733c19b363d4325286ca701fb5acc362
Franky Wijanarko <frank.jr.619@gmail.com>
2021-08-17 13:09:13 +0700

    Basic Column

--- app/src/main/java/com/fjr619/composebeginner/MainActivity.kt
+++ app/src/main/java/com/fjr619/composebeginner/MainActivity.kt
@@ -3,15 +3,20 @@ package com.fjr619.composebeginner
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.fjr619.composebeginner.ui.theme.ComposeBeginnerTheme
 
@@ -23,19 +28,53 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background,
                 modifier = Modifier.fillMaxSize()) {
-                    Greeting("Android")
+                    BasicColumn()
                 }
             }
         }
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+fun BasicColumn() {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .verticalScroll(state = ScrollState(0))) {
+        Image(
+            painterResource(R.drawable.happy_meal_small),
+            contentDescription = "",
+            modifier = Modifier.height(300.dp).fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
+
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(
+                text = "Happy Meal",
+                fontSize = 26.sp
+            )
+            Spacer(modifier = Modifier.padding(top = 10.dp))
+            Text(
+                text = "800 Calories",
+                fontSize = 17.sp
+            )
+            Spacer(modifier = Modifier.padding(top = 10.dp))
+            Text(
+                text = "$5.99",
+                color = Color(0xFF85bb65),
+                fontSize = 14.sp
+            )
+        }
+    }
+}
+
+
+
 @Composable
 fun Greeting(name: String) {
     Text("Hello World $name", Modifier.fillMaxWidth())
 }
 
-@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeBeginnerTheme {
