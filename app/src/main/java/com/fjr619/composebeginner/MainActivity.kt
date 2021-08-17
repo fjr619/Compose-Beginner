a4c77533933d894529465a7b7a2b97b4378fd52b
Franky Wijanarko <frank.jr.619@gmail.com>
2021-08-17 13:13:59 +0700

    Basic Row

--- app/src/main/java/com/fjr619/composebeginner/MainActivity.kt
+++ app/src/main/java/com/fjr619/composebeginner/MainActivity.kt
@@ -5,12 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -28,14 +31,13 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background,
                 modifier = Modifier.fillMaxSize()) {
-                    BasicColumn()
+                    BasicRow()
                 }
             }
         }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
 fun BasicColumn() {
     Column(modifier = Modifier
@@ -68,7 +70,61 @@ fun BasicColumn() {
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+fun BasicRow() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(state = ScrollState(0))
 
+    ) {
+        Image(
+            painterResource(R.drawable.happy_meal_small),
+            contentDescription = "",
+            modifier = Modifier.height(300.dp).fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+            ) {
+
+                //https://stackoverflow.com/a/66622843
+                Text(
+                    text = "Happy Happy",
+                    fontSize = 26.sp,
+                    modifier = Modifier.weight(1f)
+                )
+                
+                Text(
+                    text = "$5.99",
+                    color = Color(0xFF85bb65),
+                    fontSize = 14.sp,
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                )
+            }
+
+            Spacer(modifier = Modifier.padding(top = 10.dp))
+            Text(
+                text = "800 Calories",
+                fontSize = 17.sp
+            )
+
+            Spacer(modifier = Modifier.padding(top = 10.dp))
+            Button(
+                onClick = {},
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            ) {
+                Text(text = "button")
+            }
+        }
+    }
+}
 
 @Composable
 fun Greeting(name: String) {
